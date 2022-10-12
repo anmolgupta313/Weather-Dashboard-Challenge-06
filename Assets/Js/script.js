69e767dce37387d8476c45ad69ca9dbef9bfcc17
Anmol Gupta <anmol.gupta2180@gmail.com>
2022-10-12 03:36:19 -0400

    Updated javascript code. I have fetch open weather Api and append its data in the current weather data like current date, current temp, humidity,windspeed

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -22,74 +22,82 @@ searchBtn.addEventListener("click",function(event){
   citylistBtn.setAttribute("style", "width:100%; margin-top:20px; background-color:rgb(45, 84, 255); color:white; padding: 6px 0px 6px 0px; border:rgb(45, 84, 255); cursor:pointer;");
   citylistBtn.innerHTML= localStorage.getItem("city");
   cityListDiv.appendChild(citylistBtn);
+  
 
   
 
+    var mainDataDiv= document.querySelector("#main-data-div")
+    if(mainDataDiv.style.display==="none"){
+      mainDataDiv.style.display="flex";
+    }else{
+      mainDataDiv.setAttribute("style","display:flex;");
+    }
+   var requestUrl= 'https://api.openweathermap.org/data/2.5/forecast?q='+searchInputValue+'&list=5&units=metric&appid=6a85d182a1673983bda2c1950cb9dd1e';
+  // var requestUrl= "https://api.openweathermap.org/data/2.5/weather?q=" +
+  // searchInputValue +
+  // "&units=metric&appid=6a85d182a1673983bda2c1950cb9dd1e";  
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
 
-    // var cityName= document.querySelector("#city-name");
-    // var currentDate= document.querySelector("#current-date");
-    // var icon= document.querySelector("#icon");
-    // var temp= document.querySelector("#temp");
-    // var wind= document.querySelector("#wind");
-    // var humidity= document.querySelector("#humidity");
-    // var uvIndex= document.querySelector("#un-index");
+      console.log(data);
+
+   var fiveDayData= data.daily;
+   console.log(fiveDayData)
+
+
+    var cityName= document.querySelector("h3");
+    cityName.innerHTML= searchInputValue;
+    var currentDate= document.querySelector("#current-date");
+    // console.log(currentDate);
+    var icon= document.querySelector("#icon");
+    var temp= document.querySelector("#temp");
+    var wind= document.querySelector("#wind");
+    var humidity= document.querySelector("#humidity");
+    var uvIndex= document.querySelector("#un-index");
 
     // // console.log(cityName, currentDate, icon, temp, wind, humidity, uvIndex);
 
-    // cityName.innerHTML= nameValue;
-    // currentDate.innerHTML= "";
-    // temp.innerHTML=tempValue;
-    // wind.innerHTML= windValue;
-    // humidity.innerHTML=humidityValue;
+  
+
+    var presentDate= data.list[0].dt_txt;
+    var cityTemp= data.list[0].main.temp;
+    // console.log(cityTemp);
+    var cityWindSpeed= data.list[0].wind.speed;
+    // console.log(cityWindSpeed)
+    var cityHumidity= data.list[0].main.humidity;
+    // console.log(cityHumidity)
+    var tempIcon= data.list[0].weather[0].icon;
+
+    var urlll= "http://openweathermap.org/img/wn/";
+    // console.log(tempIcon) 
+    currentDate.innerHTML= presentDate;
+
+    temp.innerHTML="Temp: " + cityTemp + "°C";
+    wind.innerHTML= "Wind: " + cityWindSpeed + "MPH";
+    humidity.innerHTML="Humidity: " + cityHumidity +"%";
+    icon.innerHTML=tempIcon;
     // uvIndex.innerHTML=uV;
 
+    
+   for(var i=0; i<data.list.length; i= i+8){
+    var futureDate= document.querySelector(".futuree-date");
+    console.log(futureDate);
 
+    for( var v= 0; v<futureDate.length; v++){
+      
+    }
 
+    var vv= data.list[i].dt_txt;
 
+    console.log(vv);
+
+  futureDate.innerHTML= vv;
+     }
+
+  });
 
 })
-
-
-
-
-
-
-
-
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-openWeather-Key': '6a85d182a1673983bda2c1950cb9dd1e',
-// 		'X-openWeather-Host': 'https://api.openweathermap.org'
-// 	}
-// };
-
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=", options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-
-    // function getWeather(city) {
-    //     var apiUrl =
-    //       "https://api.openweathermap.org/data/2.5/weather?q=" +
-    //       city +
-    //       "&units=imperial&appid=6a85d182a1673983bda2c1950cb9dd1e";
-      
-    //     var response = fetch(apiUrl);
-    //     console.log(response);
-    //     if (response.ok) {
-    //       var data = response.json();
-    //       var nameValue = data.name;
-    //       var tempValue = data.main.temp;
-    //       var humidityValue = data.main.humidity;
-    //       var windValue = data.wind.speed;
-    //       console.log(data);
-    //       var lat = data.coord.lon;
-    //       var lon = data.coord.lat;
-    //       uvIndex(data.coord.lat, data.coord.lon);
-    //       var icon = data.weather[0].icon;}
-    //     }
-
-
 
